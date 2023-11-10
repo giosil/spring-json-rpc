6009f453992cbf32fcd55160717123785a2e8d86
giosil <giorgio.silvestris@gmail.com>
2023-11-10 17:05:10 +0100

    update

--- src/main/java/org/dew/springjsonrpc/JsonRpcController.java
+++ src/main/java/org/dew/springjsonrpc/JsonRpcController.java
@@ -1,5 +1,8 @@
 package org.dew.springjsonrpc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,17 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class JsonRpcController {
 
+  Logger logger = LoggerFactory.getLogger(JsonRpcController.class);
+  
   static {
     JsonRpc.addHandler("DEMO", new org.dew.demo.Demo());
   }
 
   @GetMapping("/")
   public String index() {
+    logger.info("index...");
+    
     return "JsonRpcController is runnnig.";
   }
   
   @PostMapping("/rpc")
   public JsonRpcResponse invoke(@RequestBody JsonRpcRequest request) {
+    logger.info("invoke...");
+    
     return JsonRpc.invoke(request);
   }
 
